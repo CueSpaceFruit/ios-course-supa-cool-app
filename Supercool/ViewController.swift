f687d01ac19e53851ca38a0ec7d397b9f6aa1f86
Cue Lai <cuelai@gmail.com>
2016-10-05 23:09:37 +1000

    added files for supercool app

--- Supercool/ViewController.swift
+++ Supercool/ViewController.swift
@@ -2,24 +2,38 @@
 //  ViewController.swift
 //  Supercool
 //
-//  Created by Jacque Lai on 27/05/2016.
+//  Created by Cue Lai on 27/05/2016.
 //  Copyright © 2016 cLai. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Interstella: UIImageView!
 
+    @IBOutlet weak var coolBG: UIImageView!
+    
+    @IBOutlet weak var makeSuperCool: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onBtnPressed(sender: AnyObject) {
+        coolBG.hidden = false
+        Interstella.hidden = false
+        makeSuperCool.hidden = true
+        
+    
+    }
 
 }
 
